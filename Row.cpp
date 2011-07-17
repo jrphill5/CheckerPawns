4bf8c010366f4241071b61cc71c735533f45f34b
Jay Phillips <rocketman8473@gmail.com>
2011-07-17 15:49:22 -0400

    filled in Row class

--- Row.cpp
+++ Row.cpp
@@ -1,10 +1,41 @@
 #include <SDL/SDL.h>
 #include "Row.h"
 
-Row::Row( int size );
+Row::Row( int ycoord, int size, int type )
 {
 
 	settings = Settings::CreateInstance();
+	this->ycoord = ycoord;
 	this->size = size;
+	this->type = type;
+
+	tiles.resize( size );
+
+	for ( int i = 0 ; i < size ; i++ )
+	{
+		if ( type == TILE_BLACK )
+			tiles[i] = new Tile( i, ycoord, ( i % 2 ) ? TILE_BLACK : TILE_WHITE );
+		else if ( type == TILE_WHITE )
+			tiles[i] = new Tile( i, ycoord, ( i % 2 ) ? TILE_WHITE : TILE_BLACK );
+		else tiles[i] = new Tile( i, ycoord, type );
+	}
 
 }
+
+void Row::clear()
+{
+	for ( int i = 0 ; i < size ; i++ )
+		tiles[i]->set_type( TILE_NONE );
+}
+
+Tile* Row::get_tile( int index )
+{
+	return tiles[ index ];
+}
+
+void Row::show(SDL_Surface* tileset, SDL_Surface* screen, vector<SDL_Rect> clips)
+{
+	for ( int i = 0 ; i < this->size ; i++ )
+		tiles[i]->show(tileset, screen, clips);
+}
+
--- Row.h
+++ Row.h
@@ -8,10 +8,17 @@ class Row
 {
 
 	private:
+		int ycoord;
 		int size;
+		int type;
+		vector< Tile* > tiles;
+		Settings* settings;
 	public:
-		Row( int size );
+		Row( int ycoord, int size, int type );
+		void clear();
+		Tile* get_tile( int index );
+		void show(SDL_Surface* tileset, SDL_Surface* screen, vector<SDL_Rect> clips);
 
-}
+};
 
 #endif
